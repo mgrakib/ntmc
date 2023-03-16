a91268f4f117bfa8b352310c4fd95d6f74b3b2de
https://github.com/mgrakib <mgrakibbd@gmail.com>
2023-03-16 23:05:45 +0600

    add dashboard

--- script/login.js
+++ script/login.js
@@ -1,18 +1,71 @@
-// document
-// 	.getElementById("agreeInputCheckBox")
-// 	.addEventListener("click", function () {
-// 		const agreeInputCheckBox =
-//             document.getElementById("agreeInputCheckBox");
-// 		const loginBtnDiv =
-//             document.getElementById("loginBtnDiv");
+/** @format */
+
+// getElementById
+const getELement = id => {
+	const element = document.getElementById(id);
+	return element;
+};
+
+const agreeInputCheckBox = () => {
+	const agreeInputCheckBox = getELement("agreeInputCheckBox");
+	const loginBtnDiv = getELement("loginBtnDiv");
+
+	if (agreeInputCheckBox.checked === true) {
+		loginBtnDiv.classList.remove("hidden");
+	} else {
+		loginBtnDiv.classList.add("hidden");
+	}
+};
+
+const loginFuncation = () => {
+    // get input value 
+	const loginUserName = getELement("loginUserName").value;
+	const loginPassword = getELement("loginPassword").value;
+    
+    // set input value blank 
+    getELement("loginUserName").value = '';
+    getELement("loginPassword").value = '';
+	
+    // get localstorage value
+	const localStorageValue = JSON.parse(
+		localStorage.getItem("registationInfo")
+    );
+   
+    const userInfo = localStorageValue.find(
+		user => Object.keys(user)[0] === loginUserName
+	);
+    
+
+    if (userInfo) {
+        document.getElementById("alartUserId").classList.add("hidden");
+
+        if (userInfo[loginUserName].userPassword != loginPassword) {
+
+            document
+				.getElementById("alartUserPassword")
+				.classList.remove("hidden");
+                
+        } else {
+             document
+					.getElementById("alartUserPassword")
+					.classList.add("hidden");
+            console.log('login successfull');
+            
+        }
         
-//         if (agreeInputCheckBox.checked === true) {
-//             loginBtnDiv.classList.remove('hidden');
-//         } else {
-//             loginBtnDiv.classList.add("hidden");
-//         }
-// 	});
+            
+    } else {
+        document.getElementById("alartUserId").classList.remove('hidden');
+    }
+    
+    
+    
+    
+    
+    
+}
 
-
-
-
+const goToSingUp = () => {
+    location.href =  '../singup.html'
+    
+}
--- script/singup.js
+++ script/singup.js
@@ -1,48 +1,63 @@
-// getElementById
-const getELement = (id) => {
-    const element = document.getElementById(id);
-    return element;
-}
 
 
 // registationSubmit function
 const registationSubmit = () => {
-    // get value from input 
-    const registatonFullName = getELement("registatonFullName").value;
-    const registatonUserName = getELement("registatonUserName").value;
-    const registatonPassword = getELement("registatonPassword").value;
+	// get value from input
+	const registatonFullName = getELement("registatonFullName").value;
+	const registatonUserName = getELement("registatonUserName").value;
+	const registatonPassword = getELement("registatonPassword").value;
+	// [{ mgrakibbd: { password: "123" } }, { popy: { password: "123" } }];
 
-    // make an object by input value 
-    const registationInfo = { registatonFullName, registatonUserName, registatonPassword }
-    
-    // get localstorage value 
-    const localStorageValue = JSON.parse(localStorage.getItem('registationInfo'));
+	// make an object by input value
+	// const registationInfo = {
+	// 	registatonFullName,
+	// 	registatonUserName,
+	// 	registatonPassword,
+	// };
 
+	const registationInfo = {
+		[registatonUserName]: {
+			userFullName: registatonFullName,
+			userPassword: registatonPassword,
+		},
+	};
 
-    if (localStorageValue) {
+	// get localstorage value
+	const localStorageValue = JSON.parse(
+		localStorage.getItem("registationInfo")
+	);
 
-        // check input user name is exist in previous localStorageValue
-        let isExist = localStorageValue.find(
-            ans => ans.registatonUserName == registatonUserName
-        );
+	if (localStorageValue) {
+		// check input user name is exist in previous localStorageValue
+		let isExist = localStorageValue.find(
+			ans => ans.registatonUserName == registatonUserName
+		);
 
-         // if exist code stope here 
-        if (isExist) {
-            alert('alreay have');
-            return;
-        }
-            
-        localStorage.setItem(
+		// if exist code stope here
+		if (isExist) {
+			alert("alreay have");
+			return;
+		}
+
+		localStorage.setItem(
 			"registationInfo",
 			JSON.stringify([...localStorageValue, registationInfo])
 		);
-            
-    } else {
-        localStorage.setItem(
+	} else {
+		localStorage.setItem(
 			"registationInfo",
 			JSON.stringify([registationInfo])
 		);
-    }
+	}
 
-    
+	// get localstorage value
+	const localStorageValueToLogin = JSON.parse(
+		localStorage.getItem("registationInfo")
+	);
+	registationInfoArray = [...localStorageValueToLogin];
+}
+
+
+const goToSingIn = () => {
+	location.href = '../login.html'
 }
